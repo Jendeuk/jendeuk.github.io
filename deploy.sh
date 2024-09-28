cb3d3df5965642e9f1c03c3c7ace897bf8a5eed8
Jenny Ngo Luong <jennyngoluong@gmail.com>
2024-09-28 21:59:12 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-curl -s https://bulky-expectations-340118.framer.app/sitemap.xml | grep https | grep -oP '(?<=<loc>).*?(?=</loc>)' | while read -r url; do wget -m -p -E -k "$url"; done
+URL="https://bulky-expectations-340118.framer.app"
+FOLDER=$(echo "$URL" | sed 's~https://~~')
 
-echo "jenny.ngoluong.no" > bulky-expectations-340118.framer.app/CNAME
+# [+] Download files from sitemap
+rm -rf "$FOLDER"
+curl -s "$URL/sitemap.xml" | grep https | grep -oP '(?<=<loc>).*?(?=</loc>)' | while read -r url; do wget -m -p -E -k "$url"; done
 
-touch bulky-expectations-340118.framer.app/.nojekyll
+# [+] Hide framer badge
+find . -type f -exec sed -i 's/id="__framer-badge-container" style="display: none;">/id="__framer-badge-container" style="display: none;">/g' {} \;
+
+# [+] Setup github config
+echo "jenny.ngoluong.no" > "$FOLDER/CNAME"
+touch "$FOLDER/.nojekyll"
